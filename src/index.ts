e5b3e8f116a9e1072f9167b77ffa1a5c79f00325
esnode85 <esnodejs@gmail.com>
2020-02-22 11:02:30 +0200

    adjusting all scripts for dev and production

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 console.log("hello I am  node app from typescript");
 console.log("hello I am  node app from typescript");
 
-class name {
-  constructor(parameters) { }
+class Name {
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+    console.log("my name is ==> ", this.name);
+  }
+  public setName(name: string): void {
+    this.name = name;
+    console.log("my name is ==> ", this.name);
+  }
 }
+
+const myName = new Name(`eslam`);
+myName.setName("farida");
